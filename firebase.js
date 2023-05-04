7e35fc1153ab768e21eb38b9d01cd64d5e480bcf
jolinefrisk <112376484+jolinefrisk@users.noreply.github.com>
2023-05-04 21:03:46 +0200

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,22 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBPztW_7aUczo17xztsI-coCNPUwyGw6go",
+
+  authDomain: "uxonavarar.firebaseapp.com",
+
+  projectId: "uxonavarar",
+
+  storageBucket: "uxonavarar.appspot.com",
+
+  messagingSenderId: "614854654469",
+
+  appId: "1:614854654469:web:1376aa1c17dcb608407a6c",
+
+  measurementId: "G-CTQR61YD30"
+
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
